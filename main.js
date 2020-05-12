0fc0f02828b642d74f2852cb681af0d84eae34c2
tamburkey <petem14@gmail.com>
2020-05-12 14:50:52 -0400

    initial commit

--- main.js
+++ main.js
@@ -21,7 +21,20 @@
   console.log('%cKATA 02 RESULTS:', 'background: darkblue; color: white; font-weight: bold;') // These console.logs are just to help make the console log better organized and more readable
 
 
-  /* Write your KATA 02 code in place of THIS comment. */
+  let counter2 = 2
+  while (counter2 <= 20) {
+    console.log(counter2)
+    counter2 += 2
+  }
+
+  // alternate solution
+  // let counter2 = 2
+  // while (counter2 <= 20) {
+  //   if(counter2 % 2 === 0){
+  //     console.log(counter2)
+  //   }
+  //   counter2 += 1
+  // }
 
 
 /** KATA 03 ⮕
@@ -33,7 +46,11 @@
   console.log('%cKATA 03 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
  
 
-  /* Write your KATA 03 code in place of THIS comment. */
+  let counter3 = 1
+  while (counter3 <= 20) {
+    console.log(counter3)
+    counter3 += 2
+  }
 
 
 /** KATA 04 ⮕
@@ -45,7 +62,11 @@
   console.log('%cKATA 04 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
 
 
-  /* Write your KATA 04 code in place of THIS comment. */
+  let counter4 = 5
+  while (counter4 <= 100) {
+    console.log(counter4)
+    counter4 += 5
+  }
 
 
 /** KATA 05 ⮕
@@ -57,7 +78,11 @@
   console.log('%cKATA 05 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
 
 
-  /* Write your KATA 05 code in place of THIS comment. */
+  let counter5 = 1
+  while (counter5 <= 10) {
+    console.log(counter5 * counter5)
+    counter5 += 1
+  }
 
 
 /** KATA 06 ⮕
@@ -70,7 +95,11 @@
 
 
   /* Write your KATA 06 code in place of THIS comment. */
-
+  let counter6 = 20
+  while (counter6 >= 1){
+    console.log(counter6)
+    counter6-=1
+  }
 
 /** KATA 07 ⮕
 ***
@@ -81,7 +110,11 @@
   console.log('%cKATA 07 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
 
 
-  /* Write your KATA 07 code in place of THIS comment. */
+  let counter7 = 20
+  while (counter7 >=2){
+    console.log(counter7)
+    counter7-=2
+  }
 
 
 /** KATA 08 ⮕
@@ -93,7 +126,11 @@
   console.log('%cKATA 08 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
 
 
-  /* Write your KATA 08 code in place of THIS comment. */
+  let counter8 = 19
+  while (counter8 >= 1){
+    console.log(counter8)
+    counter8-=2
+  }
 
 
 /** KATA 09 ⮕
@@ -105,7 +142,11 @@
   console.log('%cKATA 09 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
 
 
-  /* Write your KATA 09 code in place of THIS comment. */
+  let counter9 = 100
+  while (counter9 >= 5){
+    console.log(counter9)
+    counter9-=5
+  }
 
 
 /** KATA 10 ⮕
@@ -117,7 +158,11 @@
   console.log('%cKATA 10 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
 
 
-  /* Write your KATA 10 code in place of THIS comment. */
+  let counter10 = 10
+  while (counter10 >= 1){
+    console.log(counter10 * counter10)
+    counter10-=1
+  }
 
 
 
